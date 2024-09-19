a7c8b2493aee6b2d2563097b2252fd9bd6a35ae9
m <halabiran9@gmail.com>
2024-09-19 10:01:21 +0300

    txt to speech

--- DataBase.py
+++ DataBase.py
@@ -1,17 +1,29 @@
 # consts
 SURVIVOR = "survivor"
 VOLUNTEER = "volunteer"
-TYPES_OF_PROBLEMS = ["groceries", "medical", "technical", "metal"]
 NEED_PROF = "yes"
 NOT_NEED_PROF = "no"
+MALE = "m"
+FEMALE = "f"
 
+GROCERIES = "groceries"
+MED = "medical"
+TECH = "technical"
+MENTAL = "mental"
+TREMP = "driving"
+TYPES_OF_PROBLEMS = [GROCERIES, MED, TECH, MENTAL, TREMP]
+
+# Example Users
+'''
 user = {"first_name": "",
         "last_name": "",
+        "id": "",
         "Gender": "",
         "Location": "",
         "Age": 0,
         "profile": "",
-        "Skills": []
+        "Skills": [],
+        "matched": []
         }
 
 post = {"user": user,
@@ -21,3 +33,40 @@ post = {"user": user,
         "type of problem": "",
         "need professional": "",
         }
+'''
+
+BOB = {"first_name": "Bob",
+       "last_name": "boblovsky",
+       "id": "328273784",
+       "Gender": MALE,
+       "Location": "0213.11325,565.89786",
+       "Age": "57",
+       "profile": SURVIVOR,
+       "Skills": [],
+       "matched": []
+       }
+LINA = {"first_name": "Alina",
+        "last_name": "Segal",
+        "id": "21656548",
+        "Gender": FEMALE,
+        "Location": "2165.18548,84654.8489",
+        "Age": "45",
+        "profile": SURVIVOR,
+        "Skills": [],
+        "matched": []
+        }
+DAN = {"first_name": "Dan",
+       "last_name": "Hefetz",
+       "id": "146165454",
+       "Gender": MALE,
+       "Location": "216165.45487,132154.4554",
+       "Age": "19",
+       "profile": VOLUNTEER,
+       "Skills": [TECH],
+       "matched": [LINA]
+       }
+USERS = [BOB, LINA, DAN]
+
+
+
+
